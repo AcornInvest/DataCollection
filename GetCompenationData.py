a781d89b6fc451f3b98e9adc36ad435c1edb72aa
AcornInvest <99735066+AcornInvest@users.noreply.github.com>
2024-03-21 13:52:30 +0900

    * GetCompensation.py Log 구현중

--- GetCompenationData.py
+++ GetCompenationData.py
@@ -1,7 +1,22 @@
 import logging
+from LogStringHandler import LogStringHandler
 from Intelliquant import Intelliquant
 
 logger = logging.getLogger('GetCompensationData')
+logger.setLevel(logging.INFO)
+# 3 formatter 지정하여 log head를 구성해줍니다.
+## asctime - 시간정보
+## levelname - logging level
+## funcName - log가 기록된 함수
+## lineno - log가 기록된 line
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - [%(funcName)s:%(lineno)d] - %(message)s")
+text_handler = LogStringHandler(self.LogText)
+text_handler.setFormatter(formatter)
+# file_handler_info = logging.FileHandler(filename="log_info.log")
+file_handler_info = logging.FileHandler(filename=self.daymanage.path_log)
+file_handler_info.setFormatter(formatter)
+logger.addHandler(text_handler)
+logger.addHandler(file_handler_info)
 
 intel = Intelliquant()
 intel.ChromeOn(logger, 'NoOfShares')
