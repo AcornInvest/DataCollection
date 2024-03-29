32b581605f255f38a1b959e752a949505c1cfb04
AcornInvest <99735066+AcornInvest@users.noreply.github.com>
2024-03-29 16:38:20 +0900

    * GetCompensationData.py run_backtest_process() 완료

--- GetCompenationData.py
+++ GetCompenationData.py
@@ -37,25 +37,23 @@ class GetCompensationData(UseIntelliquant):
             for line in file:
                 if 'list_index:' in line:
                     num_codes = int(line.split('list_index:')[1].strip())
-                    #other_data.append(('list_index', num_codes))
                 elif 'NumOfStocks:' in line:
                     num_stocks = int(line.split('NumOfStocks:')[1].strip())
-                    #other_data.append(('NumOfStocks', num_stocks))
                 elif 'load_failure_list:' in line:
-                    load_failure_stocks = line.split('load_failure_list:')[1].strip()
+                    extracted_data = line.split("load_failure_list: [")[1].split("]")[0].split(",")
+                    load_failure_stocks = [x.strip() for x in extracted_data if x.strip()]  # 비어있지 않은 요소만 추가
                     num_load_failure_stocks = len(load_failure_stocks)
-                    #other_data.append(('load_failure_list', load_failure_stocks))
                 elif 'DelistingDate_Error_list:' in line:
-                    delisting_data_error_stocks = line.split('DelistingDate_Error_list:')[1].strip()
+                    extracted_data = line.split("DelistingDate_Error_list: [")[1].split("]")[0].split(",")
+                    delisting_data_error_stocks = [x.strip() for x in extracted_data if x.strip()]
                     num_delisting_data_error_stocks = len(delisting_data_error_stocks)
-                    #other_data.append(('DelistingDate_Error_list', delisting_data_error_stocks))
                 else:
                     # 일반 데이터 처리
                     parts = line.split(',')
-                    date2 = parts[1].strip()[1:-1]  # '날짜2' 추출
-                    code = parts[2].strip()
-                    old_no_share = int(parts[3].split(':')[1].strip())  # old_no_share 추출
-                    new_no_share = int(parts[4].split(':')[1].strip())  # new_no_share 추출
+                    date2 = parts[0].split("] ")[1]  # '날짜2' 추출
+                    code = parts[1].strip()
+                    old_no_share = int(parts[2].split(':')[1].strip())  # old_no_share 추출
+                    new_no_share = int(parts[3].split(':')[1].strip())  # new_no_share 추출
 
                     # 코드에 따라 데이터 묶기
                     if code not in data_by_code:
@@ -70,8 +68,20 @@ class GetCompensationData(UseIntelliquant):
         dataframes = {}
         for code, data in data_by_code.items():
             df = pd.DataFrame(data, columns=['Date', 'OldNoShare', 'NewNoShare'])
-            dataframes[code] = df
 
+            # OldNoShare를 한 칸 위로 shift한 후 NewNoShare와 비교. 마지막 행은 제외하고.
+            df['ShiftedOldNoShare'] = df['OldNoShare'].shift(-1)
+            comparison_result_without_last = (df['ShiftedOldNoShare'][:-1] == df['NewNoShare'][:-1])
+
+            # 모든 결과가 True인지 확인하고, 아니라면 해당 행의 날짜를 str 변수에 저장
+            if not comparison_result_without_last.all():
+                # 불일치하는 행의 날짜 찾기
+                mismatched_dates = df.loc[~comparison_result_without_last, 'Date']
+                err_date = ', '.join(mismatched_dates)
+                self.logger.info(f"주식수 데이터 에러. Code: {code}, date:{err_date}")
+
+            del df['ShiftedOldNoShare']
+            dataframes[code] = df
         return dataframes
 
     def run_backtest_process(self, datemanage): # Backtest 결과를 가지고 No_shares 정보의 xlsx 파일로 처리
@@ -84,7 +94,7 @@ class GetCompensationData(UseIntelliquant):
             file_names = self.get_files_starting_with(backtest_result_folder, start_string)
 
             # 처리 결과 저장할 폴더
-            no_share_folder = self.path_compensation + '\\' + type + datemanage.workday_str + '\\'
+            no_share_folder = self.path_compensation + '\\' + type + '\\' + datemanage.workday_str + '\\'
             # 폴더가 존재하지 않으면 생성
             if not os.path.exists(no_share_folder):
                 os.makedirs(no_share_folder)
@@ -94,11 +104,12 @@ class GetCompensationData(UseIntelliquant):
                 df_no_share = self.process_backtest_result(path_backtest_result_file)
                 # 여기서 각 코드의 df 에서 old, new 맞나 체크하는 루틴이 필요하다
                 # 문제가 없으면 각각 new 데이터만 남기면 된다.
-                self.save_dfs_to_excel(df_no_share, ('_' + datemanage.workday_str), no_share_folder)
+                # 그 외 df 처리하기.
+                self.save_dfs_to_excel(df_no_share, ('_compensation_' + datemanage.workday_str), no_share_folder)
 
     def save_dfs_to_excel(self, dfs_dict, custom_string, folder):
         for code, df in dfs_dict.items():
-            filename = f"{code}_{custom_string}.xlsx"
+            filename = f"{code}{custom_string}.xlsx"
             path_file = folder + filename
             df.to_excel(path_file, index=False)
 
@@ -139,8 +150,3 @@ GetCompData = GetCompensationData()
 #GetCompData.intel.chrome_on(logger, GetCompData.page, GetCompData.name)
 #GetCompData.run_backtest_rep(datemanage) # 인텔리퀀트로 백테스트 돌려서 no_share raw data 크롤링
 GetCompData.run_backtest_process(datemanage) # 인텔리퀀트로 얻은 백테스트 raw 데이터 처리
-
-#js_code = GetCompData.make_js_code(datemanage, 'Delisted', 1, 0, 19)
-#js_code = GetCompData.make_js_code(datemanage, 'Delisted', 1, 5, 10)
-#GetCompData.intel.update_code(js_code)
-#GetCompData.intel.backtest(datemanage.startday_str, datemanage.workday_str, '10000000', logger)
