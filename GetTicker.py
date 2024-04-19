557628126e2eddb5993dab372eba8d63d2050d49
AcornInvest <99735066+AcornInvest@users.noreply.github.com>
2024-04-19 14:15:39 +0900

    * GetTicker.py process_tickerlist() exception list 중 있는 엑셀 파일만 적용하도록 수정

--- GetTicker.py
+++ GetTicker.py
@@ -113,88 +113,91 @@ class GetTicker:
             date_str = '2024-03-29' # exception_list에 적용되는 날짜 str. 나중에는 각 파일마다 따로 찾는 루틴 있어야 함.
             #  스팩 우회 상장 목록 불러오기
             file_read_path = self.path_codeLists + f'\\{type_list}\\exception_list\\{type_list}_Ticker_{date_str}_스팩우회상장.xlsx'
-            stocks_spac = pd.read_excel(file_read_path, index_col=0)
-            stocks_spac['Code'] = stocks_spac['Code'].astype(str)
-            stocks_spac['Code'] = stocks_spac['Code'].str.zfill(6)  # 코드가 6자리에 못 미치면 앞에 0 채워넣기
-            stocks_spac['ListingDate'] = pd.to_datetime(stocks_spac['ListingDate']).dt.strftime('%Y-%m-%d')
-            stocks_spac['DelistingDate'] = pd.to_datetime(stocks_spac['DelistingDate']).dt.strftime('%Y-%m-%d')
-            # stocks df 에 stocks_spac_df 합성
-            stocks = stocks.merge(stocks_spac, on='Code', how='left', suffixes=('', '_spac'))
-            #  stocks_spac의 유효한 행만 stocks에 덮어 씌우기
-            stocks.loc[stocks['ListingDate_spac'].notna(), 'ListingDate'] = stocks['ListingDate_spac']
-            stocks.loc[stocks['DelistingDate_spac'].notna(), 'DelistingDate'] = stocks['DelistingDate_spac']
-            # 불필요한 열 삭제
-            stocks = stocks.drop(columns=['Name_spac', 'ListingDate_spac', 'DelistingDate_spac', '설명'])
+            if os.path.exists(file_read_path):
+                stocks_spac = pd.read_excel(file_read_path, index_col=0)
+                stocks_spac['Code'] = stocks_spac['Code'].astype(str)
+                stocks_spac['Code'] = stocks_spac['Code'].str.zfill(6)  # 코드가 6자리에 못 미치면 앞에 0 채워넣기
+                stocks_spac['ListingDate'] = pd.to_datetime(stocks_spac['ListingDate']).dt.strftime('%Y-%m-%d')
+                stocks_spac['DelistingDate'] = pd.to_datetime(stocks_spac['DelistingDate']).dt.strftime('%Y-%m-%d')
+                # stocks df 에 stocks_spac_df 합성
+                stocks = stocks.merge(stocks_spac, on='Code', how='left', suffixes=('', '_spac'))
+                #  stocks_spac의 유효한 행만 stocks에 덮어 씌우기
+                stocks.loc[stocks['ListingDate_spac'].notna(), 'ListingDate'] = stocks['ListingDate_spac']
+                stocks.loc[stocks['DelistingDate_spac'].notna(), 'DelistingDate'] = stocks['DelistingDate_spac']
+                # 불필요한 열 삭제
+                stocks = stocks.drop(columns=['Name_spac', 'ListingDate_spac', 'DelistingDate_spac', '설명'])
 
             #  제외 목록 불러오기 - 상장 폐쇄형 펀드, 스팩, 주식수 zero
             file_read_path = self.path_codeLists + f'\\{type_list}\\exception_list\\{type_list}_Ticker_{date_str}_제외목록.xlsx'
-            stocks_excepted = pd.read_excel(file_read_path, index_col=0)
-            stocks_excepted['Code'] = stocks_excepted['Code'].astype(str)
-            stocks_excepted['Code'] = stocks_excepted['Code'].str.zfill(6)  # 코드가 6자리에 못 미치면 앞에 0 채워넣기
-            #  제외 목록을 원래 list에서 삭제
-            stocks = stocks[~stocks['Code'].isin(stocks_excepted['Code'])]
+            if os.path.exists(file_read_path):
+                stocks_excepted = pd.read_excel(file_read_path, index_col=0)
+                stocks_excepted['Code'] = stocks_excepted['Code'].astype(str)
+                stocks_excepted['Code'] = stocks_excepted['Code'].str.zfill(6)  # 코드가 6자리에 못 미치면 앞에 0 채워넣기
+                #  제외 목록을 원래 list에서 삭제
+                stocks = stocks[~stocks['Code'].isin(stocks_excepted['Code'])]
 
             # KONEX에서 이전상장 목록 불러오기
             file_read_path = self.path_codeLists + f'\\{type_list}\\exception_list\\{type_list}_Ticker_{date_str}_KONEX에서_이전상장.xlsx'
-            stocks_KONEX = pd.read_excel(file_read_path, index_col=0)
-            stocks_KONEX['Code'] = stocks_KONEX['Code'].astype(str)
-            stocks_KONEX['Code'] = stocks_KONEX['Code'].str.zfill(6)  # 코드가 6자리에 못 미치면 앞에 0 채워넣기
-            stocks_KONEX['ListingDate'] = pd.to_datetime(stocks_KONEX['ListingDate']).dt.strftime('%Y-%m-%d')
-            stocks_KONEX['DelistingDate'] = pd.to_datetime(stocks_KONEX['DelistingDate']).dt.strftime('%Y-%m-%d')
-            #  KONEX 에 해당하는 기록의 목록을 원래 list에서 삭제
-            stocks_KONEX_filtered = stocks_KONEX[stocks_KONEX['설명'].str.contains('KONEX')] # stocks_KONEX에서 '설명' 칼럼에 'KONEX'가 포함된 행들을 찾음
-            # stocks_KONEX_filtered의 'Code', 'ListingDate', 'DelistingDate'를 기준으로 stocks에서 일치하는 행을 삭제
-            stocks = stocks.merge(stocks_KONEX_filtered, on=['Name', 'Code', 'ListingDate', 'DelistingDate'], how='left', indicator=True)
-            stocks = stocks[stocks['_merge'] == 'left_only'].drop(columns=['_merge'])
-            stocks = stocks.drop(columns=['설명'])  # '설명' 열 삭제
+            if os.path.exists(file_read_path):
+                stocks_KONEX = pd.read_excel(file_read_path, index_col=0)
+                stocks_KONEX['Code'] = stocks_KONEX['Code'].astype(str)
+                stocks_KONEX['Code'] = stocks_KONEX['Code'].str.zfill(6)  # 코드가 6자리에 못 미치면 앞에 0 채워넣기
+                stocks_KONEX['ListingDate'] = pd.to_datetime(stocks_KONEX['ListingDate']).dt.strftime('%Y-%m-%d')
+                stocks_KONEX['DelistingDate'] = pd.to_datetime(stocks_KONEX['DelistingDate']).dt.strftime('%Y-%m-%d')
+                #  KONEX 에 해당하는 기록의 목록을 원래 list에서 삭제
+                stocks_KONEX_filtered = stocks_KONEX[stocks_KONEX['설명'].str.contains('KONEX')] # stocks_KONEX에서 '설명' 칼럼에 'KONEX'가 포함된 행들을 찾음
+                # stocks_KONEX_filtered의 'Code', 'ListingDate', 'DelistingDate'를 기준으로 stocks에서 일치하는 행을 삭제
+                stocks = stocks.merge(stocks_KONEX_filtered, on=['Name', 'Code', 'ListingDate', 'DelistingDate'], how='left', indicator=True)
+                stocks = stocks[stocks['_merge'] == 'left_only'].drop(columns=['_merge'])
+                stocks = stocks.drop(columns=['설명'])  # '설명' 열 삭제
 
             # KOSDAQ에서 KOSPI로 이전상장 목록 불러오기
             file_read_path = self.path_codeLists + f'\\{type_list}\\exception_list\\{type_list}_Ticker_{date_str}_KOSDAQ에서_이전상장.xlsx'
-            stocks_KOSDAQ = pd.read_excel(file_read_path, index_col=0)
-            stocks_KOSDAQ['Code'] = stocks_KOSDAQ['Code'].astype(str)
-            stocks_KOSDAQ['Code'] = stocks_KOSDAQ['Code'].str.zfill(6)  # 코드가 6자리에 못 미치면 앞에 0 채워넣기
-            stocks_KOSDAQ['ListingDate'] = pd.to_datetime(stocks_KOSDAQ['ListingDate']).dt.strftime('%Y-%m-%d')
-            stocks_KOSDAQ['DelistingDate'] = pd.to_datetime(stocks_KOSDAQ['DelistingDate']).dt.strftime('%Y-%m-%d')
-            # Code별로 그룹화 후 병합 함수 적용
-            stocks_KOSDAQ_merged = pd.DataFrame()
-            for code, group in stocks_KOSDAQ.groupby('Code'):
-                merged_group = self.merge_rows(group)
-                stocks_KOSDAQ_merged = pd.concat([stocks_KOSDAQ_merged, merged_group], ignore_index=True)
+            if os.path.exists(file_read_path):
+                stocks_KOSDAQ = pd.read_excel(file_read_path, index_col=0)
+                stocks_KOSDAQ['Code'] = stocks_KOSDAQ['Code'].astype(str)
+                stocks_KOSDAQ['Code'] = stocks_KOSDAQ['Code'].str.zfill(6)  # 코드가 6자리에 못 미치면 앞에 0 채워넣기
+                stocks_KOSDAQ['ListingDate'] = pd.to_datetime(stocks_KOSDAQ['ListingDate']).dt.strftime('%Y-%m-%d')
+                stocks_KOSDAQ['DelistingDate'] = pd.to_datetime(stocks_KOSDAQ['DelistingDate']).dt.strftime('%Y-%m-%d')
+                # Code별로 그룹화 후 병합 함수 적용
+                stocks_KOSDAQ_merged = pd.DataFrame()
+                for code, group in stocks_KOSDAQ.groupby('Code'):
+                    merged_group = self.merge_rows(group)
+                    stocks_KOSDAQ_merged = pd.concat([stocks_KOSDAQ_merged, merged_group], ignore_index=True)
 
-            # stocks_KOSDAQ_merged 에서 '설명' 열 제거
-            stocks_KOSDAQ_merged = stocks_KOSDAQ_merged.drop(columns=['설명'])
-            # stocks_KOSDAQ_merged의 Code 리스트
-            codes_to_replace = stocks_KOSDAQ_merged['Code'].unique()
-            # stocks에서 해당 Code를 가진 행을 제거
-            stocks = stocks[~stocks['Code'].isin(codes_to_replace)]
-            # stocks_KOSDAQ_merged의 데이터를 stocks에 추가
-            stocks = pd.concat([stocks, stocks_KOSDAQ_merged], ignore_index=True)
+                # stocks_KOSDAQ_merged 에서 '설명' 열 제거
+                stocks_KOSDAQ_merged = stocks_KOSDAQ_merged.drop(columns=['설명'])
+                # stocks_KOSDAQ_merged의 Code 리스트
+                codes_to_replace = stocks_KOSDAQ_merged['Code'].unique()
+                # stocks에서 해당 Code를 가진 행을 제거
+                stocks = stocks[~stocks['Code'].isin(codes_to_replace)]
+                # stocks_KOSDAQ_merged의 데이터를 stocks에 추가
+                stocks = pd.concat([stocks, stocks_KOSDAQ_merged], ignore_index=True)
 
             # 상폐후 재상장 목록 불러오기
             file_read_path = self.path_codeLists + f'\\{type_list}\\exception_list\\{type_list}_Ticker_{date_str}_상폐후_재상장.xlsx'
-            stocks_relisted = pd.read_excel(file_read_path, index_col=0)
-            stocks_relisted['Code'] = stocks_relisted['Code'].astype(str)
-            stocks_relisted['Code'] = stocks_relisted['Code'].str.zfill(6)  # 코드가 6자리에 못 미치면 앞에 0 채워넣기
-            stocks_relisted['ListingDate'] = pd.to_datetime(stocks_relisted['ListingDate']).dt.strftime('%Y-%m-%d')
-            stocks_relisted['DelistingDate'] = pd.to_datetime(stocks_relisted['DelistingDate']).dt.strftime('%Y-%m-%d')
-            # 동일한 Code를 갖는 그룹에 modify_code 함수 적용
-            stocks_relisted = stocks_relisted.groupby('Code').apply(self.modify_code).reset_index(drop=True)
+            if os.path.exists(file_read_path):
+                stocks_relisted = pd.read_excel(file_read_path, index_col=0)
+                stocks_relisted['Code'] = stocks_relisted['Code'].astype(str)
+                stocks_relisted['Code'] = stocks_relisted['Code'].str.zfill(6)  # 코드가 6자리에 못 미치면 앞에 0 채워넣기
+                stocks_relisted['ListingDate'] = pd.to_datetime(stocks_relisted['ListingDate']).dt.strftime('%Y-%m-%d')
+                stocks_relisted['DelistingDate'] = pd.to_datetime(stocks_relisted['DelistingDate']).dt.strftime('%Y-%m-%d')
+                # 동일한 Code를 갖는 그룹에 modify_code 함수 적용
+                stocks_relisted = stocks_relisted.groupby('Code').apply(self.modify_code).reset_index(drop=True)
 
-            # '예전상장정보'를 포함하는 stocks_relisted의 행 필터링
-            filtered_relisted = stocks_relisted[stocks_relisted['설명'].str.contains('예전상장정보')]
-            # stocks_relisted의 '예전상장정보' 목록들로 stocks 객체 업데이트
-            for idx, relisted_row in filtered_relisted.iterrows():
-                mask = (
-                        (stocks['Name'] == relisted_row['Name']) &
-                        (stocks['ListingDate'] == relisted_row['ListingDate']) &
-                        (stocks['DelistingDate'] == relisted_row['DelistingDate'])
-                )
-                stocks.loc[mask, 'Code'] = relisted_row['Code']
-                stocks.loc[mask, 'Name'] = relisted_row['Name']
-                stocks.loc[mask, 'ListingDate'] = relisted_row['ListingDate']
-                stocks.loc[mask, 'DelistingDate'] = relisted_row['DelistingDate']
-            # '설명' 열 제거
-            #stocks.drop(columns='설명', inplace=True)
+                # '예전상장정보'를 포함하는 stocks_relisted의 행 필터링
+                filtered_relisted = stocks_relisted[stocks_relisted['설명'].str.contains('예전상장정보')]
+                # stocks_relisted의 '예전상장정보' 목록들로 stocks 객체 업데이트
+                for idx, relisted_row in filtered_relisted.iterrows():
+                    mask = (
+                            (stocks['Name'] == relisted_row['Name']) &
+                            (stocks['ListingDate'] == relisted_row['ListingDate']) &
+                            (stocks['DelistingDate'] == relisted_row['DelistingDate'])
+                    )
+                    stocks.loc[mask, 'Code'] = relisted_row['Code']
+                    stocks.loc[mask, 'Name'] = relisted_row['Name']
+                    stocks.loc[mask, 'ListingDate'] = relisted_row['ListingDate']
+                    stocks.loc[mask, 'DelistingDate'] = relisted_row['DelistingDate']
 
             # list 상장일 기준 정렬
             stocks = stocks.sort_values(by='ListingDate')  # 상장일 기준 오름차순 정렬
