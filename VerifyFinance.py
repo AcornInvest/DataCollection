b72fecbfe5f3182fc8b4bb453b4c5b3506fe3b30
AcornInvest <myoungho.kim.2013@gmail.com>
2025-05-12 11:41:59 +0900

    * VerufyFinance.py 수정 완료. 확인 필요

--- VerifyFinance.py
+++ VerifyFinance.py
@@ -7,6 +7,8 @@ from VerifyData import VerifyData
 from datetime import date, datetime, timedelta
 import numpy as np
 
+## 20250512 finance 데이터 받아오는 정보 대폭 수정함
+
 class VerifyFinance(VerifyData):
     '''
     Finance data의 무결성 검증용 child class
@@ -17,7 +19,7 @@ class VerifyFinance(VerifyData):
         self.path_data = paths.Financial
 
         self.date_prefix = 'financial_day_ref' # date reference 파일의 접미사
-        self.db_columns = ['date', 'rv', 'gp', 'oi', 'np', 'ev_evitda', 'per', 'pbr', 'psr', 'pcr', 'gpa', 'roa', 'roe']
+        self.db_columns = ['date', 'sec', 'te', 'cap', 'rv', 'cfo', 'cogs', 't_a', 'np', 'tl', 'oi', 'ie', 'ev', 'ebitda', 'ebit', 'nl', 'capex', 'depre', 'rnd', 'inven', 'ca', 'cl', 'r_e']
 
     def load_config(self):
         super().load_config()
@@ -27,7 +29,7 @@ class VerifyFinance(VerifyData):
         df_data['date'] = pd.to_datetime(df_data['date']).dt.date
         no_error = True
 
-        # 무결성 검사 2. NaN 있는지 확인
+        # 무결성 검사 1. NaN 있는지 확인
         rows_with_nan = df_data.isna().any(axis=1)  # NaN 있는지 확인
         if rows_with_nan.any():
             NaN_exists = df_data[rows_with_nan]['date'].apply(lambda d: d.strftime('%Y-%m-%d')).tolist()
@@ -38,7 +40,7 @@ class VerifyFinance(VerifyData):
             utils.save_list_to_file_append(NaN_exists_list, path)  # 텍스트 파일에 오류 부분 저장
             no_error = False
 
-        # 무결성 검사 3. 시간적 일관성 확인
+        # 무결성 검사 2. 시간적 일관성 확인
         path = f"{self.path_data}\\{datemanage.workday_str}\\time_unconsistency_list.txt"
         #path = f"{self.path_data}\\{listed_status}\\{datemanage.workday_str}_merged\\time_unconsistency_list.txt" # 임시
         # ref 와 지금 받아온 finance의 date 비교
@@ -69,37 +71,59 @@ class VerifyFinance(VerifyData):
             no_error = False
         # df_data.drop(['Out_of_Order'], axis=1, inplace=True) #처리를 어떻게 할지는 생각해 보자
 
-        # 무결성 검사 4. outlier 검출 - 음수 있는지 확인(RV)
-        # Check for negative values in RV column
-        outliers = df_data[df_data["rv"] < 0]
+        # 무결성 검사 3. outlier 검출 - 음수 있는지 확인
+        # 음수가 될 수 없는 열 목록(매출 rv 포함)
+        columns_cannot_be_negative = [
+            'rv',  # 매출
+            'cogs',  # 매출원가
+            'ie',  # 이자비용
+            'inven',  # 재고자산
+            'ca',  # 유동자산
+            'cl',  # 유동부채
+            'tl',  # 총부채
+            't_a',  # 총자산
+            'depre',  # 감가상각비
+            'rnd',  # 연구개발비
+            'capex',  # 자본적 지출
+            'cap'  # 시가총액
+        ]
 
-        if not outliers.empty:
-            outliers = outliers['date'].apply(lambda d: d.strftime('%Y-%m-%d')).tolist()
-            outliers_list = [f'{code}, rv 음수: {outliers}']
-            self.logger.info(outliers_list)
+        outliers_list = []
+
+        for col in columns_cannot_be_negative:
+            outliers = df_data[df_data[col] < 0]
+            if not outliers.empty:
+                # 날짜 리스트로 변환
+                outlier_dates = outliers['date'].apply(lambda d: d.strftime('%Y-%m-%d')).tolist()
+                # 로그 및 리스트에 기록
+                msg = f'{code}, {col} 음수: {outlier_dates}'
+                outliers_list.append(msg)
+                self.logger.info(msg)
+                no_error = False  # 오류 플래그
+
+        # outliers_list가 비어있지 않으면 파일에 기록
+        if outliers_list:
             path = f"{self.path_data}\\{datemanage.workday_str}\\outliers_list.txt"
-            #path = f"{self.path_data}\\{listed_status}\\{datemanage.workday_str}_merged\\outliers_list.txt" # 임시
-            utils.save_list_to_file_append(outliers_list, path)  # 텍스트 파일에 오류 부분 저장
-            no_error = False
+            utils.save_list_to_file_append(outliers_list, path)
 
-        # 무결성 검사 5. # 연속적으로 같은 값을 가지는지 여부를 판별
-        # 값이 두번 연속 같은 경우 검출 - 모든 항목에 대해서. 0인 경우는 제외
-        consecutive_rows = []
-        columns = ["rv", "gp", "oi", "np"]
+        # 무결성 검사 4. # 연속적으로 같은 값을 가지는지 여부를 판별
+        # 값이 두번 연속 같은 경우 검출
+        check_cols  = ['rv', 'cogs', 'oi', 'ebit', 'ebitda', 'np', 'cfo', 'cap', 'ev']
 
-        for col in columns:
-            for i in range(1, len(df_data)):
-                if df_data.loc[i, col] == df_data.loc[i - 1, col] and df_data.loc[i, col] != 0:
-                    consecutive_rows.append(df_data.iloc[i])
+        consecutive_same_values_list = []
 
-        consecutive_same_values = pd.DataFrame(consecutive_rows)
-        if not consecutive_same_values.empty:
-            consecutive_same_values = consecutive_same_values['date'].apply(lambda d: d.strftime('%Y-%m-%d')).tolist()
-            consecutive_same_values_list = [f'{code}, 값이 2 분기 연속 같은 경우: {consecutive_same_values}']
-            self.logger.info(consecutive_same_values_list)
+        for col in check_cols:  # 경고 항목은 제외
+            mask = (df_data[col] == df_data[col].shift(1)) & (df_data[col] != 0)
+            if mask.any():
+                dates = df_data.loc[mask, 'date'].dt.strftime('%Y-%m-%d').tolist()
+                msg = f'{code}, {col} ERROR: 2 분기 연속 동일 → {dates}'
+                consecutive_same_values_list.append(msg)
+                self.logger.info(msg)
+                no_error = False  # 오류 플래그
+
+        # 결과가 있으면 파일에 추가(append) 저장
+        if consecutive_same_values_list:
             path = f"{self.path_data}\\{datemanage.workday_str}\\consecutive_same_values_list.txt"
-            #path = f"{self.path_data}\\{listed_status}\\{datemanage.workday_str}_merged\\consecutive_same_values_list.txt"  # 임시
-            utils.save_list_to_file_append(consecutive_same_values_list, path)  # 텍스트 파일에 오류 부분 저장
-            no_error = False
+            utils.save_list_to_file_append(consecutive_same_values_list, path)
 
         return no_error
