b14377bf90d45e6071da99004a74929cff16b0b5
AcornInvest <99735066+AcornInvest@users.noreply.github.com>
2025-05-26 21:34:46 +0900

    * GetFinancialData.py 버그 수정함. sector 누락 추가 시험 필요

--- GetFinancialData.py
+++ GetFinancialData.py
@@ -21,7 +21,7 @@ class GetFinancialData(UseIntelliquant):
         # 인텔리퀀트 시뮬레이션 종목수 조회시 한번에 돌리는 종목 수.
         #self.max_unit_year = 1500  # 한 종목, 1년을 시뮬레이션할 때가 1 유닛. 100유닛만큼 끊어서 시뮬레이션 하겠다는 의미. 특성 4가지 할 때의 값
         #self.max_unit_year = 500 # 특성 12가지일 때.
-        self.max_unit_year = 150  # 특성 22가지일 때.
+        self.max_unit_year = 220  # 특성 22가지일 때.
         self.path_base_code = self.cur_dir + '\\' + 'get_financials_base.js'
         self.suffix = 'financial'  # 파일 이름 저장시 사용하는 접미사
         self.date_prefix = 'financial_day_ref'  # date reference 파일의 접미사
@@ -113,7 +113,8 @@ class GetFinancialData(UseIntelliquant):
         data_pattern = r'\[\d{4}-\d{2}-\d{2}\]\s\d{5,6}[A-Za-z]?,'
         date_pattern = r'\[(\d{4}-\d{2}-\d{2})\]'
         code_pattern = r'\] (\d{5}[A-Za-z]?|\d{6}),'
-        sec_pattern = r'[A-Z]\d{3},'
+        #sec_pattern = r'sector: ([A-Z]\d{3}),'
+        sec_pattern = r'sector:\s([A-Z]\d{3})?,'
         te_pattern = r'te: (-?\d+),'
         cap_pattern = r'cap: (-?\d+),'
         rv_pattern = r'rv: (-?\d+),'
@@ -134,7 +135,7 @@ class GetFinancialData(UseIntelliquant):
         inven_pattern = r'inven: (-?\d+),'
         ca_pattern = r'ca: (-?\d+),'
         cl_pattern = r'cl: (-?\d+),'
-        r_e_pattern = r'r_e: (-?\d+),'
+        r_e_pattern = r'r_e: (-?\d+)'
 
         with open(path_file, 'r', encoding='utf-8') as file:
             for line in file:
@@ -153,7 +154,11 @@ class GetFinancialData(UseIntelliquant):
                 elif re.search(data_pattern, line): # 일반 데이터 처리
                     date = re.search(date_pattern, line).group(1)
                     code = re.search(code_pattern, line).group(1)
-                    sec = re.search(sec_pattern, line).group(1)
+                    #sec = re.search(sec_pattern, line).group(1)
+                    match = re.search(sec_pattern, line)
+                    sec = match.group(1) if match and match.group(1) else "UNKNOWN"
+                    #if sec == "UNKNOWN":
+                    #    pass
                     te = re.search(te_pattern, line).group(1)
                     cap = re.search(cap_pattern, line).group(1)
                     rv = re.search(rv_pattern, line).group(1)
@@ -219,94 +224,3 @@ class GetFinancialData(UseIntelliquant):
             dataframes[code] = df
 
         return dataframes
-
-    ''' # 2024.3.31 과거에 사용하던 코드. 이제는 UseIntelliquant 로 통합함
-    def make_sql(self, datemanage):
-        # 처리된 excel 데이터를 모아서 SQL로 만듦.
-        # 2024.7.24 일회성 작업에 필요. 앞으로는 intelliquant backtest results를 바로 sql로 저장함
-        category = ['Delisted', 'Listed']
-
-        # 테이블 생성 쿼리
-        create_table_query = '''
-#        CREATE TABLE IF NOT EXISTS financial (
-#            stock_code TEXT,
-#            date TEXT,
-#            rv REAL,
-#            gp REAL,
-#            oi REAL,
-#            np REAL,
-#            ev_evitda REAL,
-#            per REAL,
-#            pbr REAL,
-#            psr REAL,
-#            pcr REAL,
-#            gpa REAL,
-#            roa REAL,
-#            roe REAL
-#        );
-'''
-
-        # sql 데이터 저장할 폴더가 존재하지 않으면 생성
-        #savedata_folder = f'{self.path_savedata}\\{listed_status}\\{datemanage.workday_str}\\'
-        savedata_folder = f'{self.path_savedata}\\{datemanage.workday_str}\\'
-        if not os.path.exists(savedata_folder):
-            os.makedirs(savedata_folder)
-
-        # SQLite 데이터베이스 파일 연결 (없으면 새로 생성)
-        #filename_db = f'{self.suffix}_{listed_status}_{datemanage.workday}.db'
-        filename_db = f'{self.suffix}_{datemanage.workday}.db'
-        file_path_db = savedata_folder + filename_db
-        conn = sqlite3.connect(file_path_db)
-        conn.execute(create_table_query)  # 테이블 생성
-        conn.commit()
-
-        def check_files(codes, folder, suffix):
-            file_names = utils.find_files_with_keyword(folder, suffix)  # 데이터 처리 결과 파일 목록. 특정 suffix가 포함된 파일만 골라냄
-            file_prefixes = set([name[:6] for name in file_names])  # 각 파일명의 처음 6글자 추출
-
-            if file_prefixes != codes or len(codes) != len(codes):
-                missing_in_files = codes - file_prefixes  # codelist에는 있는데 파일 이름에 없는 값 목록
-                extra_in_files = file_prefixes - codes  # 파일 이름에는 있는데 codelist에 없는 값 목록
-                # 결과를 텍스트 파일로 저장
-                missing_in_files_path = savedata_folder + 'missing_in_files_' + suffix + '_' + listed_status + '_' + datemanage.workday_str + '.txt'
-                with open(missing_in_files_path, 'w') as f:
-                    for item in missing_in_files:
-                        f.write("%s\n" % item)
-
-                extra_in_files_path = savedata_folder + 'extra_in_files_' + suffix + '_' + listed_status + '_' + datemanage.workday_str + '.txt'
-                with open(extra_in_files_path, 'w') as f:
-                    for item in extra_in_files:
-                        f.write("%s\n" % item)
-
-                print(
-                    f'{suffix} 데이터 에러. 결과가 {listed_status}_{suffix}_missing_in_files.txt와 {listed_status}_{suffix}_extra_in_files.txt에 저장되었습니다.')
-                sys.exit(1)
-            else:
-                print(f'모든 codelist의 목록이 {listed_status}_{suffix} 데이터에 포함되어 있습니다.')
-
-        for listed_status in category:
-            codelist_path = self.path_codeLists + '\\' + listed_status + '\\' + listed_status + '_Ticker_' + datemanage.workday_str + '_modified.xlsx'
-            codelist = pd.read_excel(codelist_path, index_col=0)
-            codelist['Code'] = codelist['Code'].astype(str)
-            codelist['Code'] = codelist['Code'].str.zfill(6)  # 코드가 6자리에 못 미치면 앞에 0 채워넣기
-            codes = set(codelist['Code'])  # Ticker 파일에서 가져온 Code column
-
-            # OHLCV 확인
-            #files_path = self.path_data + f'\\OHLCV\\Intelliquant\\{listed_status}\\{datemanage.workday_str}_merged'
-            folder_financial = os.path.join(self.path_data, 'Financial', listed_status, datemanage.workday_str) # 엑셀 파일 폴더 경로
-            suffix_financial = 'financial'
-            check_files(codes, folder_financial, suffix_financial)
-
-            for code in codes:
-                file_path_financial = f'{folder_financial}\\{code}_{suffix_financial}_{datemanage.workday_str}.xlsx'
-                df_financial = pd.read_excel(file_path_financial, index_col=None)
-                df_financial['stock_code'] = code  # 종목코드 열 추가
-                df_financial.to_sql('financial', conn, if_exists='append', index=False)
-
-        # 인덱스 생성 (쿼리 성능 향상)
-        conn.execute('CREATE INDEX IF NOT EXISTS idx_stock_code ON financial (stock_code);')
-        conn.execute('CREATE INDEX IF NOT EXISTS idx_date ON financial (date);')
-        conn.commit()
-        # 데이터베이스 연결 종료
-        conn.close()
-        '''
--- main_get_financial.py
+++ main_get_financial.py
@@ -7,10 +7,10 @@ from VerifyFinance import VerifyFinance
 from LoadConfig import LoadConfig
 
 filename = os.path.splitext(os.path.basename(__file__))[0]  # 실행하고 있는 스크립트 파일 이름 가져오기
-#startday = date(2000, 1, 4)
-#workday = date(2024, 3, 29)
-startday = date(2023, 12, 1) # 직전 financial 결과가 나오는 날부터로 함
-workday = date(2025, 1, 14)
+startday = date(2000, 1, 4)
+workday = date(2024, 3, 29)
+#startday = date(2023, 12, 1) # 직전 financial 결과가 나오는 날부터로 함
+#workday = date(2025, 1, 14)
 datemanage = DateManage(filename)
 datemanage.SetStartday(startday)
 datemanage.SetWorkday(workday)
@@ -34,9 +34,9 @@ paths = LoadConfig()
 GetFinancData = GetFinancialData(logger, paths, i, datemanage)
 #GetFinancData.make_txt_from_ticker(datemanage)
 #GetFinancData.intel.chrome_on(logger, GetFinancData.page, GetFinancData.name)
-#GetFinancData.run_backtest_rep(datemanage,  all_files=False, first_index=1, final_index=1) # 인텔리퀀트로 백테스트 돌려서 크롤링
+#GetFinancData.run_backtest_rep(datemanage,  all_files=False, first_index=50, final_index=167) # 인텔리퀀트로 백테스트 돌려서 크롤링
 #GetFinancData.run_backtest_rep(datemanage, all_files=True) # 인텔리퀀트로 백테스트 돌려서 크롤링
-#GetFinancData.run_backtest_process(datemanage) # 인텔리퀀트로 얻은 백테스트 raw 데이터 처리, db 파일로 만듦
+GetFinancData.run_backtest_process(datemanage) # 인텔리퀀트로 얻은 백테스트 raw 데이터 처리, db 파일로 만듦
 
-verify_finance = VerifyFinance(logger, paths)
-verify_finance.check_data(datemanage)
+#verify_finance = VerifyFinance(logger, paths)
+#verify_finance.check_data(datemanage)
